805137f07c484b3355fea59848e11ded7b0f2d6a
AyalaKaguya <ayalakaguya@outlook.com>
2024-11-17 22:13:55 +0800

    Release 1.0.1 feat: 为调试服务器添加文件传输支持

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 ## 安装依赖
 
+你需要先创建一个开发文件夹并按一下要求配置环境。
+
 ```shell
 npm init
 npm install easybangumi-jsdev
@@ -13,16 +15,26 @@ npm install easybangumi-jsdev
 
 可以到`node_modules/easybangumi-jsdev/src`下获取一份better_startup.js作为你项目的开始。如果找不到可以到[easybangumiorg/JsDev](https://github.com/easybangumiorg/JsDev)仓库的`src`目录下寻找。
 
-## 同步插件文件到纯纯看番
+如果无法正确的获得类型提示，检查是否安装了`IntelliCode`这类插件。
 
-这部分开发服务器这个版本暂时不内置，请使用VSCode的插件`Live Share`代替。
-
-## 启动开发服务器
+## 启动调试服务器
 
 ```shell
 npx ebjsdev
 ```
 
-在插件设置中将调试开关打开，并设置到开发服务器的地址即可获取插件运行的日志。
+在插件设置中将调试开关打开，并设置到调试服务器的地址即可获取插件运行的日志。
 
 ~~这服务器太简陋了~~
+
+## 同步插件文件到纯纯看番
+
+```shell
+npx ebjsdev path/to/file
+```
+
+你可以在开启调试服务器时设定一个文件路径，用于即时的传递你新修改的插件文件，这个文件会在插件服务器的根目录下获取到。
+
+你也可以在浏览器中直接打开命令行给出的地址查看文件是否被正确的传递。
+
+在`纯纯看番->更多->番源管理->扩展->上方加号->JS文件URL`中设定开发服务器地址即可快速同步插件文件到纯纯看番。
--- bin/devserver.js
+++ bin/devserver.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
 
 const http = require('http');
+const fs = require('fs');
+const os = require('os');
+
+const filePath = process.argv[2];
+var flag_file = false;
+
+if (filePath && fs.existsSync(filePath)) {
+    console.log('Focus on js plugin:', filePath);
+    flag_file = true;
+}
+
+function getLocalIPAddress() {
+    const interfaces = os.networkInterfaces();
+    const ips = Object.keys(interfaces).reduce((result, name) => {
+        const interface = interfaces[name];
+        const ip = interface.find(item => item.family === 'IPv4' && !item.internal);
+        if (ip) {
+            result.push(ip.address);
+        }
+        return result;
+    }, []);
+    return ips;
+}
 
 const server = http.createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/log') {
@@ -9,9 +32,29 @@ const server = http.createServer((req, res) => {
             body += chunk.toString();
         });
         req.on('end', () => {
-            console.log('log:', body);
+            try {
+                const jsonBody = JSON.parse(body);
+                if (jsonBody.level && jsonBody.label && jsonBody.msg) {
+                    console.log(`${jsonBody.level.toUpperCase()} [${jsonBody.label}]: ${jsonBody.msg}`);
+                } else {
+                    console.log(jsonBody);
+                }
+            } catch (e) {
+                console.log(e);
+                console.log(body);
+            }
             res.writeHead(200, { 'Content-Type': 'text/plain' });
             res.end('Data received');
+        });
+    } else if (req.method === 'GET' && req.url === '/' && flag_file) {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end(data);
         });
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -20,6 +63,11 @@ const server = http.createServer((req, res) => {
 });
 
 const port = 3000;
+
 server.listen(port, () => {
+    const listen_ips = getLocalIPAddress()
     console.log(`DevServer is running on http://localhost:${port}`);
+    listen_ips.forEach(ip => {
+        console.log(`- http://${ip}:${port}`);
+    });
 });

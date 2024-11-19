2bc170799625ffb066f2e684d1cb29462fb86d3a
AyalaKaguya <ayalakaguya@outlook.com>
2024-11-19 15:46:51 +0800

    更好的起始源完善钩子系统，修复日志系统无法正常关闭的问题，统一API表现形式，隐藏插件私有属性

--- src/better_startup.js
+++ src/better_startup.js
@@ -1,7 +1,7 @@
-// @key ayala.sbr
+// @key ayala.better_startup
 // @label 更好的起始源
-// @versionName 1.2
-// @versionCode 3
+// @versionName 1.3
+// @versionCode 4
 // @libVersion 11
 
 // 公共代码开始 ========================================
@@ -11,107 +11,30 @@ var okHttpHelper = Inject_OkhttpHelper;
 var webViewHelperV2 = Inject_WebViewHelperV2;
 var stringHelper = Inject_StringHelper;
 
-var plugin = {
-    debug_mode: preferenceHelper.get("ayala.sbr.debug", false),
-    debug_server: "http://localhost:3000",
-    preferences: new ArrayList(),
-    is_init: false,
-    _onBeforeMainTab_hooks: [],
-    onBeforeMainTab(hook) {
-        this._onBeforeMainTab_hooks.push(hook);
-    },
-    _onBeforeMainTab() {
-        this._onBeforeMainTab_hooks.forEach(hook => hook());
-    },
-    pagemap: new HashMap(),
-    page(name, callback) {
-        this.pagemap.put(name, {
-            page: null,
-            callback: callback,
-            subtab: null,
-            index: this.pagemap.keySet().size()
-        });
-    },
-    _getMainTabs() {
-        this.log.i("plugin._getMainTabs", "获取所有页面");
-        var tabs = new ArrayList();
-        this.pagemap.keySet().forEach(tab => {
-            tabs.add(this._checkSubTab(tab));
-        })
-        tabs.sort((a, b) => a.index - b.index);
-        var main_tabs = new ArrayList();
-        tabs = tabs.forEach(tab => main_tabs.add(tab.page));
-        return main_tabs;
-    },
-    _checkSubTab(mainTabLabel) {
-        var page = this.pagemap.get(mainTabLabel);
-        if (page.subtab == null) {
-            this.log.i("plugin._checkSubTab", "计算子页面：" + mainTabLabel);
-            var tab = {
-                type: 'none',
-                content: new HashMap(),
-                only(callback) {
-                    this.type = 'only';
-                    this.content = callback;
-                },
-                subpage(name, index, is_active, callback) {
-                    if (this.type == 'only') {
-                        this.log.w("plugin._checkSubTab", "页面已被设置为only，将跳过设置subpage：" + mainTabLabel);
-                        return
-                    };
-                    this.type = 'pages';
-                    this.content.put(name, {
-                        name: name,
-                        callback: callback,
-                        index: index,
-                        is_active: is_active
-                    });
-                }
-            }
-            page.callback(tab);
-            page.subtab = tab;
-            if (page.subtab.type == 'none')
-                this.log.e("plugin._checkSubTab", "页面未设置tab，这将导致在后续的过程中报错：" + mainTabLabel);
-            if (page.subtab.type == 'pages')
-                page.page = new MainTab(mainTabLabel, MainTab.MAIN_TAB_GROUP);
-            if (page.subtab.type == 'only')
-                page.page = new MainTab(mainTabLabel, MainTab.MAIN_TAB_WITH_COVER);
-            this.pagemap.put(mainTabLabel, page);
-        }
-        return page
-    },
-    _getSubTabs(mainTab) {
-        var page = this._checkSubTab(mainTab.label);
-        switch (page.subtab.type) {
-            case 'pages':
-                var tabs = new ArrayList();
-                page.subtab.content.keySet().forEach(key => {
-                    var value = page.subtab.content.get(key);
-                    tabs.add(new SubTab(value.name, value.is_active, value.index));
-                })
-                tabs.sort((a, b) => a.ext - b.ext);
-                return tabs;
-            default:
-                return new ArrayList();
-        }
-    },
-    _getContent(mainTab, subTab, page) {
-        var page = this._checkSubTab(mainTab.label);
-        switch (page.subtab.type) {
-            case 'only':
-                this.log.i("plugin._getContent", "获取页面内容：" + mainTab.label);
-                return page.subtab.content(page);
-            case 'pages':
-                this.log.i("plugin._getContent", "获取页面内容：" + mainTab.label + " - " + subTab.label);
-                var subTab = page.subtab.content.get(subTab.label);
-                return subTab.callback(page);
-        }
-        return new Pair(null, new ArrayList());
-    },
-    log: {
+function BetterPlugin(dev_config) {
+    var preferences = new ArrayList();
+    var pagemap = new HashMap();
+    var onBeforePreference_hooks = [];
+    var onBeforePreference_ready = false;
+    var onBeforeMainTab_hooks = [];
+    var onBeforeMainTab_ready = false;
+
+    preferences.add(new SourcePreference.Switch(
+        "调试模式",
+        "ayala.better_startup.debug",
+        dev_config.debug_mode
+    ));
+
+    preferences.add(new SourcePreference.Edit(
+        "调试服务器地址",
+        "ayala.better_startup.devServer",
+        dev_config.debug_server
+    ))
+
+    var log = {
         raw(object) {
-            if (plugin.debug_mode) {
-                var debug_server = preferenceHelper.get("ayala.sbr.devServer", plugin.debug_server);
+            if (preferenceHelper.get("ayala.better_startup.debug", dev_config.debug_mode) == 'true') {
+                var debug_server = preferenceHelper.get("ayala.better_startup.devServer", dev_config.debug_server);
                 var client = okHttpHelper.client
                 var requestBody = RequestBody.create(
                     MediaType.get('application/json; charset=utf-8'),
@@ -157,25 +80,123 @@ var plugin = {
             });
         }
     }
+
+    function checkSubTab(mainTabLabel) {
+        var page = pagemap.get(mainTabLabel);
+        if (page.subtab == null) {
+            log.i("plugin._checkSubTab", "计算子页面：" + mainTabLabel);
+            var tab = {
+                type: 'none',
+                content: new HashMap(),
+                only(callback) {
+                    this.type = 'only';
+                    this.content = callback;
+                },
+                subpage(name, index, is_active, callback) {
+                    if (this.type == 'only') {
+                        log.w("plugin._checkSubTab", "页面已被设置为only，将跳过设置subpage：" + mainTabLabel);
+                        return
+                    };
+                    this.type = 'pages';
+                    this.content.put(name, {
+                        name: name,
+                        callback: callback,
+                        index: index,
+                        is_active: is_active
+                    });
+                }
+            }
+            page.callback(tab);
+            page.subtab = tab;
+            if (page.subtab.type == 'none')
+                log.e("plugin._checkSubTab", "页面未设置tab，这将导致在后续的过程中报错：" + mainTabLabel);
+            if (page.subtab.type == 'pages')
+                page.page = new MainTab(mainTabLabel, MainTab.MAIN_TAB_GROUP);
+            if (page.subtab.type == 'only')
+                page.page = new MainTab(mainTabLabel, MainTab.MAIN_TAB_WITH_COVER);
+            pagemap.put(mainTabLabel, page);
+        }
+        return page
+    }
+
+    return {
+        log: log,
+        onBeforePreference(hook) {
+            onBeforePreference_hooks.push(hook);
+        },
+        onBeforeMainTab(hook) {
+            onBeforeMainTab_hooks.push(hook);
+        },
+        page(name, callback) {
+            pagemap.put(name, {
+                page: null,
+                callback: callback,
+                subtab: null,
+                index: pagemap.keySet().size()
+            });
+        },
+        _onBeforePreference() {
+            if (onBeforePreference_ready) return;
+            onBeforePreference_hooks.forEach(hook => hook(preferences));
+            onBeforePreference_ready = true;
+        },
+        _getPreference: () => preferences,
+        _onBeforeMainTab() {
+            if (onBeforeMainTab_ready) return;
+            onBeforeMainTab_hooks.forEach(hook => hook());
+            onBeforeMainTab_ready = true;
+        },
+        _getMainTabs() {
+            log.i("plugin._getMainTabs", "获取所有页面");
+            var tabs = new ArrayList();
+            pagemap.keySet().forEach(tab => {
+                tabs.add(checkSubTab(tab));
+            })
+            tabs.sort((a, b) => a.index - b.index);
+            var main_tabs = new ArrayList();
+            tabs = tabs.forEach(tab => main_tabs.add(tab.page));
+            return main_tabs;
+        },
+        _getSubTabs(mainTab) {
+            var page = checkSubTab(mainTab.label);
+            switch (page.subtab.type) {
+                case 'pages':
+                    var tabs = new ArrayList();
+                    page.subtab.content.keySet().forEach(key => {
+                        var value = page.subtab.content.get(key);
+                        tabs.add(new SubTab(value.name, value.is_active, value.index));
+                    })
+                    tabs.sort((a, b) => a.ext - b.ext);
+                    return tabs;
+                default:
+                    return new ArrayList();
+            }
+        },
+        _getContent(mainTab, subTab, page) {
+            var page = checkSubTab(mainTab.label);
+            switch (page.subtab.type) {
+                case 'only':
+                    log.i("plugin._getContent", "获取页面内容：" + mainTab.label);
+                    return page.subtab.content(page);
+                case 'pages':
+                    log.i("plugin._getContent", "获取页面内容：" + mainTab.label + " - " + subTab.label);
+                    var subTab = page.subtab.content.get(subTab.label);
+                    return subTab.callback(page);
+            }
+            return new Pair(null, new ArrayList());
+        },
+    }
 }
 
-// 何言说了，插件生命周期之后提供，那么我们就在这里初始化
-plugin.is_init = (function () {
-    plugin.preferences.add(new SourcePreference.Switch(
-        "调试模式",
-        "ayala.sbr.debug",
-        false
-    ))
-    plugin.preferences.add(new SourcePreference.Edit(
-        "调试服务器地址",
-        "ayala.sbr.devServer",
-        plugin.debug_server
-    ))
-    return true;
-})();
+var plugin = BetterPlugin({
+    // 默认调试配置，以纯纯看番内设置为准
+    debug_mode: false,
+    debug_server: "http://192.168.0.108:3000",
+});
 
 function PreferenceComponent_getPreference() {
-    return plugin.preferences;
+    plugin._onBeforePreference();
+    return plugin._getPreference();
 }
 
 function PageComponent_getMainTabs() {
@@ -192,12 +213,17 @@ function PageComponent_getContent(mainTab, subTab, page) {
 }
 
 // 项目代码开始 ========================================
-plugin.preferences.add(new SourcePreference.Selection(
-    "目标地址",
-    "ayala.sbr.url",
-    "http://example.com",
-))
 
+// 配置项，需要借助钩子实现配置项的增加
+plugin.onBeforePreference(preferences => {
+    preferences.add(new SourcePreference.Edit(
+        "目标地址",
+        "ayala.better_startup.url",
+        "http://example.com",
+    ))
+})
+
+// 单标签页面
 plugin.page("首页", tab => {
     tab.only(page => {
         stringHelper.toast("首页开始加载")
@@ -206,12 +232,24 @@ plugin.page("首页", tab => {
     })
 })
 
+// 多标签页面
 plugin.page("排期", tab => {
     var weekLabel = ["周一", "周二", "周三", "周四", "周五", "周六", "周日"];
     weekLabel.forEach((label, index) => {
         tab.subpage(label, index, true, page => {
             return new Pair(null, new ArrayList());
             // return getTimeLine(index, page);
+        })
+    })
+})
+
+plugin.onBeforeMainTab(() => {
+    // 需要异步加载的主页放在这里
+    plugin.page("我的", tab => {
+        // 网络或者IO操作
+        tab.only(page => {
+            return new Pair(null, new ArrayList());
+            // return getProfile(page);
         })
     })
 })
